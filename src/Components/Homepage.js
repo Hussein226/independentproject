7af88b54eadf2043b7133212e5e16073c66ff1a3
Hussein226 <houranihussein@gmail.com>
2020-02-06 21:54:46 -0800

    Working website...kinda

---
+++ src/Components/Homepage.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import './styles.css';
+import {Nav, Navbar, FormControl, Form, Button, Image} from "react-bootstrap";
+import Navigationbar from "./Navigationbar";
+import SearchPage from "./Searchbar";
+import Logo from "../Art/Goggle.png";
+
+
+const Background = require('../Art/Background.png');
+
+
+const divStyle = {
+    width: '100%',
+    height: '1000px',
+    backgroundImage: `url(${Background})`,
+    backgroundSize: 'cover'
+};
+
+
+
+
+export default class Homepage extends Component {
+    render(){
+        return(
+
+          <div style={divStyle}>
+
+            <Navigationbar/>
+
+           <img className="homepage_logo" src={Logo}/>
+           <SearchPage/>
+
+
+          </div>
+
+
+        );
+    }
+}
