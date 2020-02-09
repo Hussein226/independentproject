ee49f1754b643186d6cd826dcb312e24c4d251f5
Hussein226 <houranihussein@gmail.com>
2020-02-09 01:42:35 -0500

    Rewrote navigation bar

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -4,6 +4,7 @@ import {Nav, Navbar, FormControl, Form, Button, Image} from "react-bootstrap";
 import Navigationbar from "./Navigationbar";
 import SearchPage from "./Searchbar";
 import Logo from "../Art/Goggle.png";
+import Aboutus from "./Aboutus";
 
 
 const Background = require('../Art/Background.png');
@@ -26,7 +27,6 @@ export default class Homepage extends Component {
           <div style={divStyle}>
 
             <Navigationbar/>
-
            <img className="homepage_logo" src={Logo}/>
            <SearchPage/>
 
